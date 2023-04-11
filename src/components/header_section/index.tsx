966912c33b247c52544a056f8c8d8b4c8adc25a4
ChilikinAM <it.chilam@gmail.com>
2023-04-11 18:43:10 +0600

    Шрифты +

--- src/components/header_section/index.tsx
+++ src/components/header_section/index.tsx
@@ -7,9 +7,9 @@ const Header_section = ({header}: { header: IHeaderSektion}) => {
         <div className={styles.HeaderSection_component}>
             <div className={styles.HeaderSection_block}>
                 <p className={styles.HeaderSection_preTitle}>{header.fields.preTitle}</p>
-                <p className={styles.HeaderSection_Title}>{header.fields.title}</p>
+                <h2 className={styles.HeaderSection_Title}>{header.fields.title}</h2>
                 <p className={styles.HeaderSection_Description}>{header.fields.description}</p>
-                <Link href={header.fields.linkBtn}><div className={styles.HeaderSection_Btn}>{header.fields.btnName}</div></Link>
+                <Link href={`${header.fields.linkBtn}`}><div className={styles.HeaderSection_Btn}>{header.fields.btnName}</div></Link>
             </div>
 
         </div>
---
+++ src/components/spaltige_section/index.tsx
@@ -0,0 +1,21 @@
+import styles from "@/styles/SupportSection.module.css";
+import {documentToReactComponents} from "@contentful/rich-text-react-renderer";
+
+
+const Spaltige_section = () => {
+    return (
+        <div className={styles.SupportSection_component}>
+            <div className={styles.SupportSection_block}>
+                <h3></h3>
+                <div>documentToReactComponents()</div>
+                <div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Spaltige_section
